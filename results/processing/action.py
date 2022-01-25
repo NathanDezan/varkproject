f23b6e5a2cf12ca0ac896315aad2b7a0345ec042
Dezan <36449909+NathanDezan@users.noreply.github.com>
2022-01-25 19:40:09 -0400

    insights

--- results/processing/action.py
+++ results/processing/action.py
@@ -1,14 +1,33 @@
 import pandas as pd
 import preprocessing
 import user
+import numpy as np
+import matplotlib.pyplot as plt
 
 df_names = preprocessing.nameProcessing()
 df_emails = preprocessing.emailProcessing()
-df_test = pd.DataFrame(columns=['ID', 'Nomes', 'E-mail', 'Respostas'])
+df_results = preprocessing.responsesProcessing()
+list_vark, result_vark = preprocessing.vark_values(df_results)
+
+df_test = pd.DataFrame(columns=['ID', 'Names', 'E-mail', 'v-a-r-k', 'Learning'])
 df_test['ID'] = list(range(1, 12))
-df_test.set_index('ID')
+df_test = df_test.set_index('ID')
 
-df_test['Nomes'] = df_names['Nomes']
+#Resultados coletados
+df_test['Names'] = df_names['Nomes']
 df_test['E-mail'] = df_emails['E-mail']
+df_test['v-a-r-k'] = list_vark
+df_test['Learning'] = result_vark
 
-preprocessing.responsesProcessing()
+print(df_test)
+#print(df_results)
+
+#Insights gerados a partir dos dados
+
+# #Quantidade de pessoas por aprendizagem
+# fig = plt.figure()
+# ax = fig.add_axes([0,0,1,1])
+# vark = ['v', 'a', 'r', 'k']
+# vark_qtd = [4, 8, 1, 2]
+# ax.bar(vark, vark_qtd)
+# plt.show()
--- results/processing/preprocessing.py
+++ results/processing/preprocessing.py
@@ -4,13 +4,24 @@ def nameProcessing():
     load_names = pd.read_csv("..\\names.csv", header=None)
     names = {'ID': list(range(1, 12)), 'Nomes': load_names[0]}
     df_names = pd.DataFrame(names)
-    df_names.set_index('ID', inplace=True)
+    df_names = df_names.set_index('ID')
     # print(df_names)
 
     return df_names
 
+def emailProcessing():
+    load_emails = pd.read_csv("..\\email.csv", header=None)
+    emails = {'ID': list(range(1, 12)), 'E-mail': load_emails[0]}
+    df_emails = pd.DataFrame(emails)
+    df_emails = df_emails.set_index('ID')
+
+    return df_emails
+
 def responsesProcessing():
     load_responses = pd.read_csv("..\\mapVark.csv", header=None)
+    results = {'ID': list(range(1, 17))}
+    df_results  = pd.DataFrame(results)
+    df_results = df_results.set_index('ID')
     df_responses = pd.DataFrame(load_responses)
     itens_n = list()
     count = 0
@@ -19,7 +30,6 @@ def responsesProcessing():
 
     for i in list(range(0, 11)):
         for j in df_responses.iloc[i]:
-            print(j)
             itens_n.append(j)
             count += 1
             if count == 4:
@@ -29,50 +39,60 @@ def responsesProcessing():
         responses.append(response_n_id.copy())
         response_n_id.clear()
         
-    #     questions.append(df_responses.iloc[i])
-    #     count += 1
-    #     if count == 4:
-    #         response_n_id.append(questions.copy())
-    #         questions.clear()
-    #         count = 0
-    # responses.append(response_n_id.copy())
+    dd = dict()
+    dd['Results'] = responses
+    #print(dd['Results'][0])
+    df_results[1] = dd['Results'][0]
+    df_results[2] = dd['Results'][1]
+    df_results[3] = dd['Results'][2]
+    df_results[4] = dd['Results'][3]
+    df_results[5] = dd['Results'][4]
+    df_results[6] = dd['Results'][5]
+    df_results[7] = dd['Results'][6]
+    df_results[8] = dd['Results'][7]
+    df_results[9] = dd['Results'][8]
+    df_results[10] = dd['Results'][9]
+    df_results[11] = dd['Results'][10]
 
-    # print(responses)
+    return df_results
 
-            
-# load_csv = pd.read_csv("..\\mapVark.csv", header=None)
-# df = pd.DataFrame(load_csv)
+def vark_values(input_data):
+    load_vark = pd.read_csv("..\\vark.csv", header=None)
+    results_1 = {'id': 1, 'v': 5, 'a': 4, 'r': 3, 'k': 3}
+    results_2 = {'id': 2, 'v': 2, 'a': 7, 'r': 4, 'k': 6}
+    results_3 = {'id': 3, 'v': 11, 'a': 10, 'r': 10, 'k': 9}
+    results_4 = {'id': 4, 'v': 2, 'a': 7, 'r': 6, 'k': 5}
+    results_5 = {'id': 5, 'v': 5, 'a': 4, 'r': 3, 'k': 6}
+    results_6 = {'id': 6, 'v': 5, 'a': 8, 'r': 0, 'k': 5}
+    results_7 = {'id': 7, 'v': 6, 'a': 10, 'r': 2, 'k': 5}
+    results_8 = {'id': 8, 'v': 6, 'a': 6, 'r': 5, 'k': 5}
+    results_9 = {'id': 9, 'v': 6, 'a': 3, 'r': 2, 'k': 5}
+    results_10 = {'id': 10, 'v': 6, 'a': 10, 'r': 5, 'k': 9}
+    results_11 = {'id': 11, 'v': 1, 'a': 5, 'r': 4, 'k': 5}
+    vark_results = [results_1, results_2, results_3, results_4, results_5, results_6, results_7, results_8, results_9, results_10, results_11]
 
-# count = 0
-# questions = list()
-# response_n_id = list()
-# responses = list()
+    results_vark = ['v', 'a', 'v a r', 'a', 'k', 'a', 'a', 'v a', 'v', 'a', 'a k']
 
-# for i in df.iloc[1]:
-#     count += 1
-#     questions.append(i)
-#     if count == 4:
-#         response_n_id.append(questions.copy())
-#         questions.clear()
-#         count = 0
+    return vark_results, results_vark
 
-# responses.append(response_n_id.copy())
 
-# #print(responses[0])
+#   var position = writeData.getLastRow();
+#   var mapVarkOccurrence = [];
 
-# # #Pre-processing dataframe
-# # df.set_index()
-# # print(df)
-# # print(df.iloc[0])
-# # response_1 = convertResponse(df.iloc[0])
-
-# # for i in response_1:
-# #     print(i)
-
-def emailProcessing():
-    load_emails = pd.read_csv("..\\email.csv", header=None)
-    emails = {'ID': list(range(1, 12)), 'E-mail': load_emails[0]}
-    df_emails = pd.DataFrame(emails)
-    df_emails.set_index('ID', inplace=True)
-
-    return df_emails
+#   for(var i = 0; i < 64; i++){
+#     mapVarkOccurrence.push(writeData.getRange(position, i+1).getValue().toString());
+#   }
+#   for(var j = 0; j < mapVarkOccurrence.length; j++){
+#     if(mapVarkOccurrence[j] == 1){
+#       if(mapVarkLetter[j] == "V"){
+#         v++;
+#       }else if(mapVarkLetter[j] == "A"){
+#         a++;
+#       }else if(mapVarkLetter[j] == "R"){
+#         r++;
+#       }else if(mapVarkLetter[j] == "K"){
+#         k++;
+#       }
+#     }
+#   }
+#   Logger.log(v + " | " + a + " | " + r + " | " + k);
