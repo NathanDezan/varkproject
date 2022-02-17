665465cfaff00ff60742a276d22c13fac2591a4f
Dezan <36449909+NathanDezan@users.noreply.github.com>
2022-02-17 17:58:24 -0400

    Refatorando código

---
+++ v2/back/index.js
@@ -0,0 +1,12 @@
+var express = require('express');
+var app = express();
+const path = require('path');
+app.use(express.static(path.join(__dirname, '..', '/front')));
+
+app.listen(8080, () => {
+    console.log("Application listening on port 8080...");
+});
+
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname, '../front/index.html'));
+});
---
+++ v2/front/js/collectData.js
@@ -0,0 +1,8 @@
+
+function collectResponses(){
+    var otherCheckbox = document.getElementById("C01K");
+
+    console.log(otherCheckbox);
+}
+
+collectResponses();
